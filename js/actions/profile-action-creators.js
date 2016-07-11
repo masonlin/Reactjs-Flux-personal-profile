b52333589159073a34bb42d7e93f6959f991406b
Mason Lin (賣森林) <104554013@cc.ncu.edu.tw>
2016-07-12 00:24:37 +0800

    for socket.io

--- js/actions/profile-action-creators.js
+++ js/actions/profile-action-creators.js
@@ -2,15 +2,12 @@ var AppDispatcher = require('../dispatcher/app-dispatcher');
 
 module.exports = {
 
-  //createProfile: function(comment) {
   createProfile: function() {
     var action = {
       actionType: "CREATE_PROFILE"
-      //,
-      //comment: comment
     };
 
-    console.log("dispatch==");
+    //console.log("dispatch==");
     AppDispatcher.dispatch(action);
   }
 };
